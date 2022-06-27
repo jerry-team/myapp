b6e2258e9dd85c7859268332d1b71b7987f5d47d
Jerry <719622388@qq.com>
2022-06-27 16:31:30 +0800

    xy首页与后端接口连接

--- app/src/main/java/com/jerry/myapp/api/ApiConfig.java
+++ app/src/main/java/com/jerry/myapp/api/ApiConfig.java
@@ -2,11 +2,11 @@ package com.jerry.myapp.api;
 
 public class ApiConfig {
     public static final int PAGE_SIZE = 5;
-<<<<<<< HEAD
-    public static final String BASE_URl = "http://192.168.212.42:8001";
-=======
-    public static final String BASE_URl = "http://192.168.1.71:8001";
->>>>>>> ecfc4eb85b716ed0c7d8d11e50a2a99b47c99345
+
+    public static final String BASE_URl = "http://10.22.132.31:8001";
+
+
     public static final String LOGIN = "/user/login"; //登录
     public static final String REGISTER = "/user/register"; //注册
+    public static final String CATEGORY_LIST = "/category/list";//类别查询
 }
